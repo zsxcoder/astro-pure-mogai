8fb7da8110c769079a9ab613edfb9c63f156f3f3
Artemisia <114164438+artemisia1107@users.noreply.github.com>
2025-10-07 22:18:02 +0800

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // === Basic configuration ===
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'Astro Theme Pure',
+  title: "Li's Blog",
   /** Will be used in index page & copyright declaration */
   author: 'Pure Lab',
   /** Description metadata for your website. Can be used in page metadata. */
