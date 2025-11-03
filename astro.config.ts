907452e820fb3907abdb2bd431982d572791483f
xlinkexe <2457444033@qq.com>
2025-11-03 22:19:55 +0800

    chore: 更新站点URL为blog子域名
    将主站点URL从https://ljx.icu更改为https://blog.ljx.icu，以使用专门的博客子域名

--- astro.config.ts
+++ astro.config.ts
@@ -26,7 +26,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://ljx.icu',
+  site: 'https://blog.ljx.icu',
   // Deploy to a sub path; See https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   // base: '/astro-pure/',
   trailingSlash: 'never',
