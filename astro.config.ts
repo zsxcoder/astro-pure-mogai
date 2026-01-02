5aa2340575ff151d863698eb78525bddb96683b5
xlinkexe <2457444033@qq.com>
2026-01-02 21:31:47 +0800

    chore: remove unused font configuration from astro config

--- astro.config.ts
+++ astro.config.ts
@@ -112,20 +112,23 @@ export default defineConfig({
     svgo: true,
     // Enable font preloading and optimization
     // https://docs.astro.build/en/reference/experimental-flags/fonts/
-    fonts: [
-      {
-        provider: fontProviders.fontshare(),
-        name: 'Satoshi',
-        cssVariable: '--font-satoshi',
+
+    // no use for now
+
+    // fonts: [
+    //   {
+    //     provider: fontProviders.fontshare(),
+    //     name: 'Satoshi',
+    //     cssVariable: '--font-satoshi',
         // Default included:
         // weights: [400],
         // styles: ["normal", "italics"],
         // subsets: ["cyrillic-ext", "cyrillic", "greek-ext", "greek", "vietnamese", "latin-ext", "latin"],
         // fallbacks: ["sans-serif"],
-        styles: ['normal', 'italic'],
-        weights: [400, 500],
-        subsets: ['latin']
-      }
-    ]
+    //     styles: ['normal', 'italic'],
+    //     weights: [400, 500],
+    //     subsets: ['latin']
+    //   }
+    // ]
   }
 })
