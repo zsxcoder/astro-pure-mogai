c413f2339f843ec3554f07a53d612978e01cf3e5
mcyzsx <mcyzsx@outlook.com>
2026-01-03 23:53:20 +0800

    chore(config): update description with tilde

--- src/site.config.ts
+++ src/site.config.ts
@@ -12,7 +12,7 @@ export const theme: ThemeUserConfig = {
   /** Will be used in index page & copyright declaration */
   author: '钟神秀',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: '造化钟神秀，阴阳割昏晓。',
+  description: '造化钟神秀，阴阳割昏晓~',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** The default social card image for your site which should be a path to an image in the `public/` directory. */
